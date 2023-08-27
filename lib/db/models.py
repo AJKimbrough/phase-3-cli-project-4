9fa5dd55ac46900da660c47c0b9f56d9082d20ae
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2023-08-27 07:24:59 -0500

    creates exercise table schema

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,7 +1,14 @@
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+from datetime import datetime
 
 Base = declarative_base()
 
 class exercise(Base):
     __tablename__ = 'exercises'
+
+    id = Column(Integer, primary_key=True)
+    exercise_name = Column(String)
+    difficulty = Column(String)
+    started_at = Column(DateTime)
+    completed_at = Column(DateTime, default=datetime.now())
