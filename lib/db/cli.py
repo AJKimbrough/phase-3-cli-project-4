f5312feffe38427cb2b998d21da5fb6f8fe66b90
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2023-08-27 22:06:04 -0500

    working on cli.py

--- lib/db/cli.py
+++ lib/db/cli.py
@@ -7,4 +7,15 @@ url = 'sqlite:///workout_data.db'
 
 engine = create_engine(url)
 Session = sessionmaker(bind=engine)
-Session()
+
+
+def cli():
+    """Workout Tracker"""
+    pass
+
+@cli.command()
+@click.argument('date')
+@click.argument('workout', nargs=1)
+def add_workout(date, workout):
+    """Add new workout"""
+    session = Session()
