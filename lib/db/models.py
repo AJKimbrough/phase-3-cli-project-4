90a30008184f51bc3f93083fb17aa22b303b1c45
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2023-08-27 20:42:34 -0500

    adds relationship to user

--- lib/db/models.py
+++ lib/db/models.py
@@ -2,13 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from datetime import datetime
+from sqlalchemy.orm import relationship, backref
 
 engine = create_engine('sqlite://workout_data')
 
 Base = declarative_base()
 
 class Workout(Base):
-    __tablename__ = 'exercises'
+    __tablename__ = 'workout'
 
     id = Column(Integer, primary_key=True)
     workout_name = Column(String)
@@ -16,7 +17,7 @@ class Workout(Base):
     started_at = Column(DateTime)
     completed_at = Column(DateTime, default=datetime.now())
 
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
 
     def __repr__(self):
         return f'workout(id={self.id}, ' + \
@@ -24,12 +25,14 @@ class Workout(Base):
             f'difficulty={self.difficulty})'
     
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
     user_name = Column(String)
     age = Column(Integer)
     sport = Column(String)
+
+    workouts = relationship("Workout", backref=backref('the_user'))
 
     def __repr__(self):
         return f'User(id={self.id}, ' + \
