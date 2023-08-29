655291283840805ce2304711d0f980cf9be389f1
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2023-08-28 20:25:54 -0500

    updates cli.py

--- lib/db/cli.py
+++ lib/db/cli.py
@@ -8,14 +8,20 @@ url = 'sqlite:///workout_data.db'
 engine = create_engine(url)
 Session = sessionmaker(bind=engine)
 
-
+@click.group()
 def cli():
     """Workout Tracker"""
     pass
 
 @cli.command()
 @click.argument('date')
-@click.argument('workout', nargs=1)
-def add_workout(date, workout):
-    """Add new workout"""
+@click.argument('user', nargs=1)
+def add_user(date, user):
+    """Add new user"""
     session = Session()
+
+    users_data = [{'user_name': user_name, 'age': age, 'sport': sport} for user_name, age, sport in (w.split(':') for w in workout)]
+    workout = Workout(date = date, workouts = [])
+
+    for workout_data in workouts_data:
+        workout = Workout()
